e5016196f4be66f5b604dc47ecea42181a7ec2a2
Phurinat Bunyachai <phurinatb@zupports.co>
2025-03-10 14:57:25 +0700

    edit: set up router

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div>ss</div>
+  <router-view v-slot="{ Component }">
+    <transition name="fade">
+      <component :is="Component" />
+    </transition>
+  </router-view>
 </template>
