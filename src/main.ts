e440efa5fe0edd686cfbbc4b5542a257b125296d
Phurinat Bunyachai <phurinatb@zupports.co>
2025-03-10 20:39:54 +0700

    enable liff

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,12 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import { router } from '@/route/router.ts'
-// import { useLiff } from '@/composables/useLiff.ts'
+import { useLiff } from '@/composables/useLiff.ts'
 
-// const { initialize } = useLiff()
+const { initialize } = useLiff()
 const app = createApp(App)
 
 app.use(router)
 app.mount('#app')
 
-// initialize()
+initialize()
