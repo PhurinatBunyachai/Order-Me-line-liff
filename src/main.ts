e891ca12b837fc1439347da376110ffdf130cef3
Phurinat Bunyachai <phurinatb@zupports.co>
2025-04-03 18:42:56 +0700

    edit: add pinia plugin

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,13 @@ import App from './App.vue'
 import { router } from '@/route/router.ts'
 import { useLiff } from '@/composables/useLiff.ts'
 import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 
 const { initialize } = useLiff()
 
 const app = createApp(App)
 const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
 
 app.use(router)
 app.use(pinia)
