5cd8be1aec7984de9b7ada41e9a1c23c56145531
Phurinat Bunyachai <phurinatb@zupports.co>
2025-04-09 15:07:30 +0700

    hf

--- vite.config.ts
+++ vite.config.ts
@@ -17,6 +17,7 @@ export default defineConfig({
       '/api/notion': {
         target: 'https://api.notion.com/v1',
         changeOrigin: true,
+        ws: true,
         rewrite: (path) => path.replace(/^\/api\/notion/, '')
       }
     }
